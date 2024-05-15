21a7babb934b7393776ba014c9852ed95775ade6
Calipride237 <layou1620@gmail.com>
2024-05-15 12:41:24 +0200

    Trying

---
+++ UserMan.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinalFinanceTrack
+{
+    /// <summary>
+    /// Interaction logic for UserMan.xaml
+    /// </summary>
+    public partial class UserMan : Window
+    {
+        public UserMan()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    Form2 f2 = new Form2();
+        //    f2.Show();
+        //    this.Hide();
+        //}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
